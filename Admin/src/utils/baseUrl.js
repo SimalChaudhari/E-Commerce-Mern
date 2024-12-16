cd0a01eb8176b10b026cbf693b4a8d52aaa8760a
zangula-dev <simalc@zangula.com>
2024-12-16 15:47:17 +0530

    add

--- Admin/src/utils/baseUrl.js
+++ Admin/src/utils/baseUrl.js
@@ -1,3 +1,3 @@
 // export const base_url = "http://localhost:5000/api/";
-export const base_url = "https://e-commerce-mern-tv4l.vercel.app/api/"
+export const base_url = "https://e-commerce-mern-beryl.vercel.app/api/"
 
